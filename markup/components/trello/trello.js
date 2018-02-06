adc470f9c6895f4a1f4dcd422ecbd7190139cafd
Roman Korenchuk <roman@markupus.com>
2018-02-06 19:45:02 +0200

    trello

--- markup/components/trello/trello.js
+++ markup/components/trello/trello.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function dragenter(e) {
-  console.log('entered ');
+  let t = e.target;
+  if (!t.classList.contains('trello-cards-item') || t.classList.contains('target')) {return;}
+  
+  let placeholder = document.createElement('div');
+  placeholder.className = 'trello-cards-item-placeholder';
+}
+
+function dragleave(e) {
+  let t = e.target;
+  if (!t.classList.contains('trello-cards-item') || t.classList.contains('target')) {return;}
+  console.log('entered', e.target);
 }
 
 function dragstart(e) {
@@ -10,19 +20,29 @@ function dragstart(e) {
   t.classList.add('target');
   // var newEl = event.target.cloneNode(true);
 
-  document.addEventListener('dragenter', dragenter);
+  if (e.target.classList.contains('trello-cards-item')) {
+    document.addEventListener('dragenter', dragenter);
+    document.addEventListener('dragleave', dragleave);
+  }
 }
 
 function drag(e) {
   let t = e.target;
-  if (t.draggable != true) {return;}
-  console.log('moving');
 }
 
 function dragend(e) {
   let t = e.target;
   if (t.draggable != true) {return;}
-  console.log('bye');
+  t.classList.remove('target');
+}
+
+function dragover(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  let t = e.target;
+  if (t.draggable != true) {return;}
   t.classList.remove('target');
 }
 
@@ -33,3 +53,5 @@ function dragend(e) {
 document.addEventListener('dragstart', dragstart);
 document.addEventListener('drag', drag);
 document.addEventListener('dragend', dragend);
+document.addEventListener('dragover', dragover);
+document.addEventListener('drop', drop);
