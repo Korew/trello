526ed1d16d1338c058f73f9e590f48bcffa7bfc2
Roman Korenchuk <ikorew@gmail.com>
2018-02-06 02:04:42 +0200

    js start

--- markup/components/trello/trello.js
+++ markup/components/trello/trello.js
@@ -1,1 +1,33 @@
+'use strict';
 
+function dragstart(e) {
+  let t = e.target;
+  if (t.draggable != true) {return;}
+  t.classList.add('target');
+  // var newEl = event.target.cloneNode(true);
+
+  // document.addEventListener('dragenter', dragenter);
+}
+
+function drag(e) {
+  let t = e.target;
+  if (t.draggable != true) {return;}
+  console.log('moving');
+}
+
+function dragend(e) {
+  let t = e.target;
+  if (t.draggable != true) {return;}
+  console.log('bye');
+  t.classList.remove('target');
+}
+
+// function dragenter(e) {
+//   console.log('entered ' t);
+// }
+
+
+
+document.addEventListener('dragstart', dragstart);
+document.addEventListener('drag', drag);
+document.addEventListener('dragend', dragend);
